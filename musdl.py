1c6f15379ca932c5abafdb0b9ee0319f5e974e31
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2020-10-03 04:18:18 +0000

    revert changes

--- musdl.py
+++ musdl.py
@@ -10,7 +10,7 @@ import bs4 as bsoup
 import requests
 
 __author__ = "Ong Yong Xin, JPFrancoia"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __copyright__ = "(c) 2020 Ong Yong Xin, JPFrancoia"
 __license__ = "MIT"
 
