ba8ca19b974b3ff34c0a31215a831c05ef31b3ae
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2021-01-29 00:47:34 +0800

    (chore) cleanup

--- musdl/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf8
-"""musdl, [mus]escore [d]own[l]oader, a Python interface to musescore-downloader"""
-
-__author__ = "Ong Yong Xin"
-__version__ = "3.0.0"
-__copyright__ = "(c) 2020 Ong Yong Xin"
-__license__ = "MIT"
